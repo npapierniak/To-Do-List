b972c341d17e5af6d91b31abddaf553a0d626d4c
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-01-25 09:36:01 -0600

    Enabled data persistence

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
         }
     }
 }
-struct ToDoItem: Identifiable {
+struct ToDoItem: Identifiable, Codable {
     var id = UUID ()
     var priority = String()
     var description = String()
